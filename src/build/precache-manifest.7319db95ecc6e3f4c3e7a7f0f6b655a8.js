994b02568de0a44a67720418d6387235a7151661
Taehyun Kim <tkim294@wisc.edu>
2020-07-29 11:46:38 -0500

    First Commit

---
+++ src/build/precache-manifest.7319db95ecc6e3f4c3e7a7f0f6b655a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cb8e885f29311bb1faaabd58c94bea8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c6ad3a8037a25100b8d",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "33803bca1eb6d23093f9",
+    "url": "/static/js/2.def41a19.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.def41a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c6ad3a8037a25100b8d",
+    "url": "/static/js/main.ffffada0.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
